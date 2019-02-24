b8a8cfecb6f998cc2f0f9653a6e917af889eac42
Ralan <rollwho@gmail.com>
2019-02-24 17:22:07 +0600

    added user roles

--- sdu_beta_career/users/models.py
+++ sdu_beta_career/users/models.py
@@ -1,16 +1,26 @@
 from typing import Dict
 
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db import models
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 
 
 class User(AbstractUser):
-
+    STUDENT = 1
+    ADVISOR = 2
+    APPROVER = 3
+    MENTOR = 4
+    USER_ROLES = (
+        (STUDENT, "student"),
+        (ADVISOR, "advisor"),
+        (APPROVER, "approver"),
+        (MENTOR, "mentor")
+    )
     # First Name and Last Name do not cover name patterns
     # around the globe.
-    name = CharField(_("Name of User"), blank=True, max_length=255)
+    name = models.CharField(_("Name of User"), blank=True, max_length=255)
+    role = models.IntegerField(choices=USER_ROLES, default=1)
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
--- sdu_beta_career/users/tests/test_models.py
+++ sdu_beta_career/users/tests/test_models.py
@@ -1,8 +1,38 @@
 import pytest
 from django.conf import settings
-
+from django.test import TestCase
+from ..models import User
 pytestmark = pytest.mark.django_db
 
 
 def test_user_get_absolute_url(user: settings.AUTH_USER_MODEL):
     assert user.get_absolute_url() == f"/users/{user.username}/"
+
+
+class TestModelUser(TestCase):
+    def test_default_user(self):
+        new_user = User()
+        new_user.name = "Test User"
+        new_user.save()
+        self.assertEqual(new_user.role, 1)
+
+    def test_advisor_user(self):
+        new_user = User()
+        new_user.name = "Test User"
+        new_user.role = 2
+        new_user.save()
+        self.assertEqual(new_user.role, 2)
+
+    def test_mentor_user(self):
+        new_user = User()
+        new_user.name = "Test User"
+        new_user.role = 4
+        new_user.save()
+        self.assertEqual(new_user.role, 4)
+
+    def test_approve_user(self):
+        new_user = User()
+        new_user.name = "Test User"
+        new_user.role = 3
+        new_user.save()
+        self.assertEqual(new_user.role, 3)
