d953bc55642e503ff26ff659f973a01c01838209
Ralan <rollwho@gmail.com>
2019-03-03 01:14:47 +0600

    template rendered

--- sdu_beta_career/users/views.py
+++ sdu_beta_career/users/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse
 from django.views.generic import DetailView, ListView, RedirectView, UpdateView
 
+from sdu_beta_career.users.models import Profile
+
 User = get_user_model()
 
 
@@ -50,3 +52,16 @@ class UserRedirectView(LoginRequiredMixin, RedirectView):
 
 
 user_redirect_view = UserRedirectView.as_view()
+
+
+class ProfileDetailView(DetailView):
+    template_name = 'users/profile_details.html'
+    model = Profile
+
+    def get_context_data(self, **kwargs):
+        context = super(ProfileDetailView, self).get_context_data(**kwargs)
+        context['data'] = Profile.objects.filter(pk=self.kwargs.get('pk'))
+        return context
+
+
+profile_detail_view = ProfileDetailView.as_view()
