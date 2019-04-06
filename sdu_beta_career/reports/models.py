64b527ad3cb81caf468fad3333d08a591e9452c7
An4ik <akhmetov.anuar09@gmail.com>
2019-04-06 17:36:47 +0600

    added app to installed apps, added migrations

--- sdu_beta_career/reports/models.py
+++ sdu_beta_career/reports/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
 class Report(models.Model):
-	
-	title = models.CharField(max_length = 100)
-	body = models.TextField()
-	mark = models.IntegerField(default=0) 
-	date = models.DateTimeField(auto_now_add = True)
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    mark = models.IntegerField(default=0)
+    date = models.DateTimeField(auto_now_add=True)
